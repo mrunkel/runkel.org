1b55f01195709ee8ad3401fd0297f98ecce16b0f
Marc Runkel <marc@runkel.org>
2017-07-21 13:31:27 +0200

    First generation of public content

---
+++ public/js/runkel.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function() {
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#000"
+      },
+      "button": {
+        "background": "#f1d600"
+      }
+    },
+    "position": "bottom-right",
+    "content": {
+      "href": "https://www.runkel.org/cookie-policy"
+    }
+  })
+});
